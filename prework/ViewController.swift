6da4ebc66be3eab03c9467213fe3b4b6577f3569
farnaf <fnafarif@ucsd.edu>
2021-04-02 16:58:16 -0700

    updated

--- prework/ViewController.swift
+++ prework/ViewController.swift
@@ -8,16 +8,57 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var view1: UIView!
+    
+    @IBOutlet weak var headerView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var themeSwitch: UISwitch!
+    
+    @IBOutlet weak var inputCardView: UIView!
     @IBOutlet weak var billAmountTextField: UITextField!
-    @IBOutlet weak var tipAmountLabel: UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
+
+    @IBOutlet weak var outputCardView: UIView!
+    @IBOutlet weak var tipAmountTitleLabel: UILabel!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var totalAmountTitleLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var resetButton: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view
     }
 
+    
+    @IBAction func themeToggled(_ sender: UISwitch) {
+        if sender.isOn {
+            headerView.backgroundColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+            inputCardView.backgroundColor = #colorLiteral(red: 0.05882352963, green: 0.180392161, blue: 0.2470588237, alpha: 1)
+            outputCardView.backgroundColor = #colorLiteral(red: 0.4508578777, green: 0.9882974029, blue: 0.8376303315, alpha: 1)
+            resetButton.backgroundColor = #colorLiteral(red: 0.05882352963, green: 0.180392161, blue: 0.2470588237, alpha: 1)
+            
+            titleLabel.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+            tipControl.backgroundColor = #colorLiteral(red: 0.4508578777, green: 0.9882974029, blue: 0.8376303315, alpha: 1)
+            tipAmountTitleLabel.backgroundColor = #colorLiteral(red: 0.05882352963, green: 0.180392161, blue: 0.2470588237, alpha: 1)
+            totalAmountTitleLabel.backgroundColor = #colorLiteral(red: 0.05882352963, green: 0.180392161, blue: 0.2470588237, alpha: 1)
+        } else {
+            headerView.backgroundColor = #colorLiteral(red: 0.9490196078, green: 0.9490196078, blue: 0.968627451, alpha: 1)
+            inputCardView.backgroundColor = #colorLiteral(red: 0, green: 0.3285208941, blue: 0.5748849511, alpha: 1)
+            outputCardView.backgroundColor = #colorLiteral(red: 0.8446564078, green: 0.5145705342, blue: 1, alpha: 1)
+            resetButton.backgroundColor = #colorLiteral(red: 0, green: 0.3285208941, blue: 0.5748849511, alpha: 1)
+            
+            titleLabel.textColor = #colorLiteral(red: 0.3333333433, green: 0.3333333433, blue: 0.3333333433, alpha: 1)
+            tipControl.backgroundColor = #colorLiteral(red: 0.8446564078, green: 0.5145705342, blue: 1, alpha: 1)
+            tipAmountTitleLabel.backgroundColor = #colorLiteral(red: 0, green: 0.3285208941, blue: 0.5748849511, alpha: 1)
+            totalAmountTitleLabel.backgroundColor = #colorLiteral(red: 0, green: 0.3285208941, blue: 0.5748849511, alpha: 1)
+            
+        }
+    }
+    
     @IBAction func calculateTip(_ sender: Any) {
         // Get bill amount from text filed input
         let bill = Double(billAmountTextField.text!) ?? 0
@@ -27,11 +68,18 @@ class ViewController: UIViewController {
         let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
         let total = bill + tip
         
-        // Update Tip Amount Label
+        // Update Tip and Total Labels
         tipAmountLabel.text = String(format: "$%.2f", tip)
-        // Update Total Amount
         totalLabel.text = String(format: "$%.2f", total)
     }
     
+    @IBAction func resetButtonTapped(_ sender: UIButton) {
+        billAmountTextField.text = nil
+        tipControl.selectedSegmentIndex = 0
+        tipAmountLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+    }
 }
+
+
 
